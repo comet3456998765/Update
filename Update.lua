35a2c80d204d6e10c278efa5a9049132e6a54ca2
comet3456998765 <109533854+comet3456998765@users.noreply.github.com>
2023-02-04 13:22:49 -0600

    Update Update.lua

--- Update.lua
+++ Update.lua
@@ -1,3 +1,1 @@
-print("No Updates bro")
-
---loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/Update/main/Newadm"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/Update/main/Newadm"))()
